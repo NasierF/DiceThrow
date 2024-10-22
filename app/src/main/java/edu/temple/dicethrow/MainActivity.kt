25407810bb95ec3abdecec5844f4435884a9e262
NasierF <119966231+NasierF@users.noreply.github.com>
2024-10-22 12:21:38 -0400

    updating remote

--- app/src/main/java/edu/temple/dicethrow/MainActivity.kt
+++ app/src/main/java/edu/temple/dicethrow/MainActivity.kt
@@ -8,17 +8,23 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.FragmentManager
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ButtonFragment.buttonInterface {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val button = findViewById<Button>(R.id.rollDiceButton)
+        //val button = findViewById<Button>(R.id.rollDiceButton)
+         supportFragmentManager.beginTransaction().add(R.id.fragmentContainerView, )
         val dieFrag = DieFragment.newInstance(100)
         supportFragmentManager.beginTransaction().add(R.id.DiceContainer, dieFrag).commit()
 
-        button.setOnClickListener {
-            (supportFragmentManager.findFragmentById(R.id.DiceContainer) as DieFragment).throwDie()
-        }
+//        button.setOnClickListener {
+//            (supportFragmentManager.findFragmentById(R.id.DiceContainer) as DieFragment).throwDie()
+//        }
 
     }
+
+    override fun buttonClicked() {
+
+    }
+
 }
